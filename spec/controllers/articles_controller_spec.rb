995190a1c857bc55688be3651a89d1d9f26c7516
MatiasEJ <don.juncos@gmail.com>
2022-01-25 20:58:24 -0300

    añade test en articles_controller

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 describe ArticlesController do
 
 	describe '#index' do
-		it 'should return success' do
+		it 'should return success response' do
 			get :index
 			expect(response).to have_http_status(:ok )
 		end
 
-		it 'should return success' do
-			create_list :article, 2
+		it 'should return proper json' do
+			create_list :article, 3
 			get :index
 			json = JSON.parse(response.body)
 			pp json
-			# json_data = json[:data]
-			# expect(json_data.length).to eq(2)	
+			json_data = json[:data]
+			expect(json_data).to eq(2)	
 		end
 
 	end
