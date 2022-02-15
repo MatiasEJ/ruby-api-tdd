cd70d5ecd8491715d016feb0e8a12b9982533d44
MatiasEJ <don.juncos@gmail.com>
2022-02-15 20:59:23 -0300

    añade service para comunicarse con github

--- lib/user_authenticator.rb
+++ lib/user_authenticator.rb
@@ -1,13 +1,4 @@
 class UserAuthenticator
-	class AuthenticationError < StandardError; end
-
-	attr_reader :user
-
-	def initialize(code)
-	end
-
-	def perform
-		raise AuthenticationError
-	end
+	
 
 end
